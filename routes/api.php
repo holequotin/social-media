57e1a406a25f68d68a69921c72888812ba2675b2
TinHLQ-Kozocom <tinhlq@kozo-japan.com>
2024-03-15 10:48:11 +0700

    fixed route list

--- routes/api.php
+++ routes/api.php
@@ -18,63 +18,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('forget_password', [AuthController::class, 'forgetPassword']);
-    Route::get('verify', [AuthController::class, 'verify']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-});
+Route::group(['middleware' => 'api'], function () {
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('forget_password', [AuthController::class, 'forgetPassword']);
+        Route::get('verify', [AuthController::class, 'verify']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
 
-Route::group([
-    'middleware' => ['api', 'auth:api'],
-], function () {
-    Route::group([
-        'prefix' => 'posts',
-        'as' => 'posts.'
-    ], function () {
-        Route::get('/', [PostController::class, 'index'])->name('index');
-        Route::post('/', [PostController::class, 'store'])->name('store');
-        Route::patch('/{post}', [PostController::class, 'update'])->name('update');
-        Route::delete('/{post}', [PostController::class, 'destroy'])->name('destroy');
+        Route::group([
+            'middleware' => 'auth:api'
+        ], function ($router) {
+            Route::post('logout', [AuthController::class, 'logout']);
+            Route::post('me', [AuthController::class, 'me']);
+            Route::post('reset_password', [AuthController::class, 'resetPassword']);
+        });
     });
 
-    Route::group([
-        'prefix' => 'comments',
-        'as' => 'comments.'
-    ], function () {
-        Route::get('/', [CommentController::class, 'index'])->name('index');
-        Route::post('/', [CommentController::class, 'store'])->name('store');
-        Route::patch('/{comment}', [CommentController::class, 'update'])->name('update');
-        Route::delete('/{comment}', [CommentController::class, 'destroy'])->name('destroy');
-    });
-
-    Route::group([
-        'prefix' => 'reactions',
-        'as' => 'reactions.'
-    ], function () {
-        Route::get('/', [ReactionController::class, 'index'])->name('index');
-        Route::post('/', [ReactionController::class, 'store'])->name('store');
-        Route::patch('/{reaction}', [ReactionController::class, 'update'])->name('update');
-        Route::delete('/{reaction}', [ReactionController::class, 'destroy'])->name('destroy');
-    });
-
-    Route::group([
-        'prefix' => 'notifications',
-        'as' => 'notifications.'
-    ], function ($router) {
-        Route::get('/', [NotificationController::class, 'index'])->name('index');
-        Route::patch('/{notification}', [NotificationController::class, 'markAsRead'])->name('markAsRead');
-    });
-
-    Route::group([
-        'prefix' => 'auth'
-    ], function ($router) {
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('me', [AuthController::class, 'me']);
-        Route::post('reset_password', [AuthController::class, 'resetPassword']);
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::group([
+            'prefix' => 'posts',
+            'as' => 'posts.'
+        ], function () {
+            Route::get('/', [PostController::class, 'index'])->name('index');
+            Route::post('/', [PostController::class, 'store'])->name('store');
+            Route::patch('/{post}', [PostController::class, 'update'])->name('update');
+            Route::delete('/{post}', [PostController::class, 'destroy'])->name('destroy');
+        });
+    
+        Route::group([
+            'prefix' => 'comments',
+            'as' => 'comments.'
+        ], function () {
+            Route::get('/', [CommentController::class, 'index'])->name('index');
+            Route::post('/', [CommentController::class, 'store'])->name('store');
+            Route::patch('/{comment}', [CommentController::class, 'update'])->name('update');
+            Route::delete('/{comment}', [CommentController::class, 'destroy'])->name('destroy');
+        });
+    
+        Route::group([
+            'prefix' => 'reactions',
+            'as' => 'reactions.'
+        ], function () {
+            Route::get('/', [ReactionController::class, 'index'])->name('index');
+            Route::post('/', [ReactionController::class, 'store'])->name('store');
+            Route::patch('/{reaction}', [ReactionController::class, 'update'])->name('update');
+            Route::delete('/{reaction}', [ReactionController::class, 'destroy'])->name('destroy');
+        });
+    
+        Route::group([
+            'prefix' => 'notifications',
+            'as' => 'notifications.'
+        ], function ($router) {
+            Route::get('/', [NotificationController::class, 'index'])->name('index');
+            Route::patch('/{notification}', [NotificationController::class, 'markAsRead'])->name('markAsRead');
+        });
     });
 });
