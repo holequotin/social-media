4a8d7aa15486732b96fb3d473fd84fd049ee7cbe
TinHLQ-Kozocom <tinhlq@kozo-japan.com>
2024-04-03 09:31:51 +0700

    update create group migration

--- database/migrations/2024_04_02_062202_create_groups_table.php
+++ database/migrations/2024_04_02_062202_create_groups_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\GroupType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,8 @@ return new class extends Migration {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('url');
+            $table->string('url')->nullable();
+            $table->enum('type', GroupType::getValues())->default(GroupType::PUBLIC);
             $table->unsignedBigInteger('owner_id');
             $table->timestamps();
 
--- database/migrations/2024_04_02_062458_create_group_user_table.php
+++ database/migrations/2024_04_02_062458_create_group_user_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
             $table->unsignedBigInteger('group_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
-
             $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
