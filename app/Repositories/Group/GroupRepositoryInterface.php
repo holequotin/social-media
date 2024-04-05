8e8d8fa4e73f67aa064677378121b92795194f4e
TinHLQ-Kozocom <tinhlq@kozo-japan.com>
2024-04-05 14:00:51 +0700

    move isInGroup, isWaitingAcceptGroup to repository

--- app/Repositories/Group/GroupRepositoryInterface.php
+++ app/Repositories/Group/GroupRepositoryInterface.php
@@ -11,6 +11,9 @@ interface GroupRepositoryInterface extends RepositoryInterface
     public function joinGroup(Group $group, User $user);
     public function leaveGroup(Group $group, User $user);
     public function requestToJoinGroup(Group $group, User $user);
-
     public function getGroupsByUser(User $user, $perPage);
+
+    public function isInGroup(Group $group, User $user);
+
+    public function isWaitingAcceptGroup(Group $group, User $user);
 }
