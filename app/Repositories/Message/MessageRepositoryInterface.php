bba4762d8a6085bc6afb0a229b7a8ca5fae08cec
TinHLQ-Kozocom <tinhlq@kozo-japan.com>
2024-04-15 08:12:34 +0700

    send message, get messages

---
+++ app/Repositories/Message/MessageRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Message;
+
+use App\Models\User;
+use App\Repositories\RepositoryInterface;
+
+interface MessageRepositoryInterface extends RepositoryInterface
+{
+    public function getMessagesBetweenUsers(User $user1, User $user2, $perPage);
+
+    public function getLastMessages(User $user);
+}
